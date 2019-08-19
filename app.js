291c40659938d688bd151434ab03be983fbccb59
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-19 21:19:50 +0530

    /read page added. Reproduces the submitted details.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const Express = require('express');
+var bodyParser =require('body-parser');
 
 var app = new Express();
 app.set('view engine', 'ejs');
 app.use(Express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     res.render('index');
+});
+
+app.post('/read',(req,res)=>{
+    var details = req.body;
+    //console.log(details);
+    res.render('read',{details});
 });
 
 app.listen(process.env.PORT || 3080,()=>{
