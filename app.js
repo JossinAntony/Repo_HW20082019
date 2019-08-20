c6e7e5abee7b5e4afd575247e6dd85fdd3916e89
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-20 15:45:47 +0530

    API defined for retrieving single student from admin no. View for search single student added.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Express = require('express');
 var bodyParser =require('body-parser');
-const Mongoose=require('mongoose');   //db connection
-var Request = require('request');
+const Mongoose=require('mongoose');   //db connection working with mongodb: 1. mdb
+var Request = require('request');     //11. mdb include request module
 
 var app = new Express();
 app.set('view engine', 'ejs');
@@ -9,10 +9,10 @@ app.set('view engine', 'ejs');
 app.use(Express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({extended:true})); // parse data from sunmission form <form></form>  in page
 
-Mongoose.connect("mongodb://localhost:27017/collegedb"); //connect the db
+Mongoose.connect("mongodb://localhost:27017/collegedb"); //connect the db   2. mdb
 //create a model (MVC), arguemnts: collection name, schema, 
 
-const StudentModel = Mongoose.model("studentdetails",
+const StudentModel = Mongoose.model("studentdetails",         // 3.mdb define the student model (including the schema) (the model in MVC)
 {uname:String,
 urollno:String,
 uadmno:String, 
@@ -26,13 +26,13 @@ app.get('/index',(req,res)=>{
     res.render('index');
 });
 
-app.post('/read',(req,res)=>{
+app.post('/read',(req,res)=>{                       // 4. mdb adding astudent action called as 'read' (as defined in <form></form> in index page                                                                submission forum) ACTION: inserting & saving data in database
     var details = req.body;
     //console.log(details);
     //res.send({details});
     //res.render('read',{details});
-    var student = new StudentModel(details);
-    var result=student.save((error, data)=>{
+    var student = new StudentModel(details);        // 5.mdb- define the instance of model created
+    var result=student.save((error, data)=>{         // 6.mdb save the data, handle error, display a message
         if(error){
             throw error;
             res.send('error');
@@ -42,8 +42,8 @@ app.post('/read',(req,res)=>{
         });
 });
 
-app.get('/getStudentData',(req,res)=>{ // this is an API, this can bve tested at postman http://localhost:3080/getStudentData, should returndata
-    var display = StudentModel.find((error, data)=>{
+app.get('/getStudentData',(req,res)=>{ //7. mdb this is an API to retieve data from database, this can be tested at postman http://localhost:3080/getStudentData, should return 'data',  can also be tested in browser
+    var display = StudentModel.find((error, data)=>{  // 8.mdb database.find() call
         if (error){
             throw error;
         }else{
@@ -52,19 +52,37 @@ app.get('/getStudentData',(req,res)=>{ // this is an API, this can bve tested at
     });
 });
 
-const getDataApi = "http://localhost:3080/getStudentData"; // this is the api link
+const getDataApi = "http://localhost:3080/getStudentData"; //9. mdb  this is the api link, accessing the api
 
-app.get('/viewstudents',(req,res)=>{
+app.get('/viewstudents',(req,res)=>{                        //10. mdb using the api
     // need an npm module 'request' to call the api url=> npm install request --save
-    Request(getDataApi, (error, response, body)=>{
+    Request(getDataApi, (error, response, body)=>{          //12.mdb API link, other stuff
         if(error){
             throw error;
         }else{
-            var data = JSON.parse(body);
+            var data = JSON.parse(body);                     //13.mdb parse body, render required view, send data; further processing at view page.
             res.render('viewstudents',{'data':data});
         }
     });
 });
+
+// Define an API to retrieve info of a single student from admno
+app.get('/getAstudentAPI/:admno',(req, res)=>{
+    var admnNo = req.params.admno;
+    StudentModel.find({uadmno:admnNo}, (error, data)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(data);
+        }
+    });
+});
+
+//define a UIView for searching student- route it
+app.get('/searchstudent',(req,res)=>{
+    res.render('searchstudent');
+});
+
 
 app.listen(process.env.PORT || 3080,()=>{
     console.log("Server running at http://localhost:3080")
